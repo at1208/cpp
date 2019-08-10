bf984778f9f6049fef1dfa65eab588c6114b23b5
aman tiwari <amantiwari12081999@gmail.com>
2019-08-10 13:30:23 +0530

    done;

---
+++ linearArray/linearArray/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+
+using namespace std;
+
+int main(){
+    int nums[5] = {11,22,33,44,55};
+    int i = 0;
+    while(i<5){
+        cout<<nums[i]<<endl;
+        i++;
+    }
+    return 0;
+}
